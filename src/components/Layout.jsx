503430ad54ebd8dc212e473c20873ce273c4ce83
Cherrypick14 <cherrypicklet@gmail.com>
2022-05-20 11:46:45 +0300

    add layout component

---
+++ src/components/Layout.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Header from './Header'
+import HeroSec from './HeroSec'
+import About from './About'
+import Contact from './Contact'
+import Footer from './Footer'
+
+const Layout = () => {
+  return <>
+    <Header />
+    <HeroSec />
+    <About />
+    <Contact />
+    <Footer />
+    </>
+  
+}
+
+export default Layout
