6d6625c99311aa4e553be12b11f703dc41f749fe
Cherrypick14 <cherrypicklet@gmail.com>
2022-05-20 11:46:19 +0300

    add hero content

---
+++ src/components/HeroSec.jsx
@@ -0,0 +1,56 @@
+import React, {useRef, useEffect} from 'react'
+import '../styles/hero-sec.css';
+
+import { Container,Row,Col } from 'reactstrap'
+
+import { init } from 'ityped'
+
+const HeroSec = () => {
+
+    const textRef = useRef()
+
+useEffect(()=>{
+
+    init(textRef.current,{
+
+         backDelay:1500,
+         showCursor:true,
+         strings:[
+             'Cheryl Petra',
+             'a Web Developer',
+             'a Technical Writer'
+         ]
+    })
+
+    }, [])
+  return <section className='hero_sec' id="home">
+      <Container>
+          <Row>
+              <Col lg='6' md='6'>
+                  <div className="hero_content">
+                      <p className='mb-3'>Get to know me</p>
+                      {/* <h5></h5> */}
+                      <h2 className='hero_title mb-4'>I'm 
+                        <span ref={textRef}></span>
+                        </h2>
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt consequatur fuga quibusdam saepe officia? Obcaecati earum porro sunt corporis molestiae! Itaque dolor tempora sint magni? Nihil beatae incidunt aperiam officia?
+
+                      </p>
+                      <div className="hero_btns d-flex align-items-center gap-4 mt-4">
+                          <button className='btn hire_btn'><a href="#">Hire me</a></button>
+                          <button className='btn contact-btn'>Contact</button>
+                      </div>
+                  </div>
+              </Col>
+              <Col lg='6' md='6'>
+                  <div className="hero-img">
+                      <img src={process.env.PUBLIC_URL + "/newkiddo.png"} className="w-100"alt="profile" />
+                  </div>
+              </Col>
+          </Row>
+      </Container>
+  </section>
+    
+}
+
+export default HeroSec
