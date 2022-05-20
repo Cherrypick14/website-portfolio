b7e2943922fb91b1e33c6a2fabfbcde852c0ad80
Cherrypick14 <cherrypicklet@gmail.com>
2022-05-20 11:46:01 +0300

    add footer content

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "../styles/footer.css"
+
+import { Container } from 'reactstrap'
+
+const Footer = () => {
+  return <footer>
+
+   <Container className='text-center footer'>
+       <p>&copy;Copyright 2022, Developed with Love by Petra , All Rights reserved</p>
+   </Container>
+  </footer>
+}
+
+export default Footer
